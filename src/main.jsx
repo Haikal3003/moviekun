da26c2f6771770633687e0b002574c9643d915fb
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-10-08 21:23:53 +0700

    feat(main): add default axios base url

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
