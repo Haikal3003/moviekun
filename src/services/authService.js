fc32c4c8c2e5a670dfc381f6b9a51b7a2c3c1a76
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-11-04 18:51:51 +0700

    feat: add auth service

---
+++ src/services/authService.js
@@ -0,0 +1,35 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../config/firebaseConfig';
+import { doc, setDoc } from 'firebase/firestore';
+
+export const registerUser = async (email, password, firstName, lastName) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    if (user) {
+      await setDoc(doc(db, 'Users', user.uid), {
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        photo: '',
+      });
+
+      console.log('User Registered Successfully!!');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const loginUser = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+    console.log('User Login Successfully!!');
+
+    return userCredential.user;
+  } catch (error) {
+    console.log(error);
+  }
+};
