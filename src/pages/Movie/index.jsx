d9dec38defce3081e4a1f538de0e86fd808dfbd6
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-11-04 18:52:44 +0700

    feat(filter): add filter movie / tv show

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -9,11 +9,11 @@ import Pagination from '../../components/Pagination';
 const MoviePage = () => {
   const [selectedGenre, setSelectedGenre] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
   const itemsPerPage = 20;
 
   const { data: movies, totalPages: totalPagesMovies } = useFetch(`/movie/popular`, currentPage);
   const { data: genres } = useFetchGenres('/genre/movie/list');
-
   const genreQuery = selectedGenre.join(',');
 
   const { data: filterMovie, totalPages: totalPagesWithGenre } = useFetchDiscover('/discover/movie', genreQuery, currentPage);
@@ -26,14 +26,25 @@ const MoviePage = () => {
   const displayMovies = selectedGenre.length > 0 ? filterMovie : movies;
   const totalPageCount = selectedGenre.length > 0 ? totalPagesWithGenre : totalPagesMovies;
 
+  const filteredMovies = displayMovies.filter((movie) => movie.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <section className="relative w-full h-auto">
       <div className="flex justify-between gap-8">
         <Filter selectedGenre={selectedGenre} setSelectedGenre={setSelectedGenre} genres={genres} media_type="Movies" />
-        <div className="grid grid-cols-5 gap-4">
-          {displayMovies.map((movie, index) => (
-            <Card key={`${movie.id}-${index}`} data={movie} media_type={'movie'} />
-          ))}
+
+        <div className="flex flex-col w-full">
+          <h1 className="mb-4 text-[1.5rem] font-semibold">Search Movie</h1>
+          <div id="search" className="flex items-center w-full bg-gray-100 mb-4 rounded-xl p-5">
+            <input type="search" placeholder="Search Movie..." className="w-full text-[1.1rem] bg-transparent outline-none" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+          </div>
+          <div className="grid grid-cols-5 gap-4">
+            {filteredMovies.length > 0 ? (
+              filteredMovies.map((movie, index) => <Card key={`${movie.id}-${index}`} data={movie} media_type={'movie'} />)
+            ) : (
+              <h1 className="absolute left-[60%] top-1/2 -translate-x-[60%] -translate-y-1/2 text-[1.2rem]">Movie Not Found</h1>
+            )}
+          </div>
         </div>
       </div>
       <Pagination totalPages={Math.ceil(totalPageCount / itemsPerPage)} currentPage={currentPage} onPageChange={handlePageChange} />
--- src/pages/TvShow/index.jsx
+++ src/pages/TvShow/index.jsx
@@ -9,6 +9,7 @@ import Pagination from '../../components/Pagination';
 const TvShowPage = () => {
   const [selectedGenre, setSelectedGenre] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
   const itemsPerPage = 20;
 
   const { data: TvShow, totalPages: totalPageTV } = useFetch(`/tv/popular`, currentPage);
@@ -16,24 +17,34 @@ const TvShowPage = () => {
 
   const genreQuery = selectedGenre.join(',');
 
-  const { data: filteredTV, totalPages: totalPagesWithGenre } = useFetchDiscover('/discover/tv', genreQuery, currentPage);
+  const { data: filterTV, totalPages: totalPagesWithGenre } = useFetchDiscover('/discover/tv', genreQuery, currentPage);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  const displayTV = selectedGenre.length > 0 ? filteredTV : TvShow;
+  const displayTV = selectedGenre.length > 0 ? filterTV : TvShow;
   const totalPageCount = selectedGenre.length > 0 ? totalPagesWithGenre : totalPageTV;
+
+  const filteredTVShow = displayTV.filter((tv) => tv.name.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <section className="relative w-full h-auto">
       <div className="flex justify-between gap-8">
         <Filter selectedGenre={selectedGenre} setSelectedGenre={setSelectedGenre} genres={genres} media_type="tv" />
-        <div className="grid grid-cols-5 gap-4">
-          {displayTV.map((tv, index) => (
-            <Card key={`${tv.id}-${index}`} data={tv} media_type={'tv'} />
-          ))}
+        <div className="flex flex-col w-full">
+          <h1 className="mb-4 text-[1.5rem] font-semibold">Search TV Show</h1>
+          <div id="search" className="flex items-center w-full bg-gray-100 mb-4 rounded-xl p-5">
+            <input type="search" placeholder="Search TV Show..." className="w-full text-[1.1rem] bg-transparent outline-none" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+          </div>
+          <div className="grid grid-cols-5 gap-4">
+            {filteredTVShow.length > 0 ? (
+              filteredTVShow.map((tv, index) => <Card key={`${tv.id}-${index}`} data={tv} media_type={'tv'} />)
+            ) : (
+              <h1 className="absolute left-[60%] top-1/2 -translate-x-[60%] -translate-y-1/2 text-[1.2rem]">TV Show Not Found</h1>
+            )}
+          </div>
         </div>
       </div>
       <Pagination totalPages={Math.ceil(totalPageCount / itemsPerPage)} currentPage={currentPage} onPageChange={handlePageChange} />
