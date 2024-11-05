fc3b93e50c8787af64b415806065ddd3b747eba6
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-11-05 15:58:13 +0700

    feat: add provider redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,24 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { UserProvider } from '../src/context/userContext';
 import router from './routes';
 import axios from 'axios';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_TMDB_ACCESS_TOKEN}`;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <PersistGate loading={null} persistor={persistor}>
+      <UserProvider>
+        <RouterProvider router={router} />
+      </UserProvider>
+    </PersistGate>
   </Provider>
 );
