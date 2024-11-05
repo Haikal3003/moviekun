deb020292a85d6c326f5c25041267f692418de5c
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-11-05 16:05:39 +0700

    chore: update

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,18 +1,29 @@
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import useFetchDetails from '../../hooks/useFetchDetails';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { FaList, FaHeart } from 'react-icons/fa';
 import { IoIosBookmark } from 'react-icons/io';
 import VideoPlay from '../../components/VideoPlay';
 import useFetch from '../../hooks/useFetch';
+import { useUserContext } from '../../context/userContext';
+import { useEffect } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../config/firebaseConfig';
+import { setFavorites } from '../../store/favoriteSlice';
+import { setWatchlist } from '../../store/watchlistSlice';
+import { addToFavorite, addToWatchlist } from '../../services/userService';
 
 const DetailPage = () => {
   const { media_type, id } = useParams();
+  const user = useUserContext();
+  const dispatch = useDispatch();
+
   const imageURL = useSelector((state) => state.movieData.imageURL);
+  const favorites = useSelector((state) => state.favoriteData);
+  const watchlist = useSelector((state) => state.watchlistData);
 
   const { data } = useFetchDetails(`${media_type}/${id}`);
   const { data: castData } = useFetchDetails(`${media_type}/${id}/credits`);
-  const { data: recommendationData } = useFetch(`${media_type}/${id}/recommendations`);
 
   const formatRuntime = (runtime) => {
     const hours = Math.floor(runtime / 60);
@@ -34,12 +45,84 @@ const DetailPage = () => {
     return amount ? `$${amount.toLocaleString()}` : 'N/A';
   };
 
+  const storeData = {
+    media_type: media_type,
+    data: data,
+  };
+
+  const handleAddFavorite = async () => {
+    if (!user.isAuthenticated) {
+      console.log('User is not authenticated');
+      return;
+    }
+
+    const isItemExist = favorites.some((item) => item.data.id === data.id);
+    if (isItemExist) {
+      console.log('Movie / TV Show already exists in favorites');
+      return;
+    }
+
+    const userId = user.userData['uid'];
+    try {
+      await addToFavorite(userId, storeData);
+      console.log('Item added to favorites');
+    } catch (error) {
+      console.error('Error adding to favorites:', error);
+    }
+  };
+
+  const handleAddWatchlist = async () => {
+    if (!user.isAuthenticated) {
+      console.log('User is not authenticated');
+      return;
+    }
+
+    const isItemExist = watchlist.some((item) => item.data.id === data.id);
+    if (isItemExist) {
+      console.log('Movie / TV Show already exists in watchlist');
+      return;
+    }
+
+    const userId = user.userData['uid'];
+
+    try {
+      await addToWatchlist(userId, storeData);
+    } catch (error) {
+      console.error('Error adding to watchlist:', error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (!user.isAuthenticated || !user.userData) {
+        console.log('User is not authenticated or userData is missing.');
+        return;
+      }
+
+      try {
+        const userDoc = await getDoc(doc(db, 'Users', user.userData.uid));
+        if (userDoc.exists()) {
+          dispatch(setFavorites(userDoc.data().favorites || []));
+          dispatch(setWatchlist(userDoc.data().watchlist || []));
+        } else {
+          console.log('User document does not exist.');
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, [user, dispatch]);
+
+  console.log(favorites);
+
   return (
     <section>
       <div>
         <div className="flex items-center">
-          <div className="max-w-[200px] w-full min-h-auto rounded-lg">
-            <img src={data?.poster_path ? imageURL + data.poster_path : '/default-poster.png'} className="w-full h-full object-cover rounded-lg" alt={data?.title || data?.name || 'Poster'} />
+          <div className="max-w-[250px] w-full min-h-auto rounded-lg">
+            <img src={imageURL + data?.poster_path} className="w-full h-full object-cover rounded-lg" alt={data?.title || data?.name || 'Poster'} />
           </div>
           <div className="ml-10">
             <div id="title" className="text-[2.6rem] flex items-center gap-4">
@@ -64,11 +147,11 @@ const DetailPage = () => {
                 <FaList />
                 <span className="tooltip">Add to list</span>
               </div>
-              <div className="icon-button group p-6 bg-gray-900 text-white rounded-full text-xl cursor-pointer relative">
+              <div className="icon-button group p-6 bg-gray-900 text-white rounded-full text-xl cursor-pointer relative" onClick={handleAddFavorite}>
                 <FaHeart />
                 <span className="tooltip">Mark as favorite</span>
               </div>
-              <div className="icon-button group p-6 bg-gray-900 text-white rounded-full text-xl cursor-pointer relative">
+              <div className="icon-button group p-6 bg-gray-900 text-white rounded-full text-xl cursor-pointer relative" onClick={handleAddWatchlist}>
                 <IoIosBookmark />
                 <span className="tooltip">Add to your watchlist</span>
               </div>
@@ -107,7 +190,7 @@ const DetailPage = () => {
             </div>
           </div>
 
-          <div className=" w-full flex justify-between gap-14 my-20 bg-gray-100 p-6 rounded-lg">
+          <div className="w-full flex justify-between gap-14 my-20 bg-gray-100 p-6 rounded-lg">
             <div className="border-r-2 border-r-gray-900 w-full">
               <h1 className="text-[1.3rem] font-semibold mb-2">Original Title</h1>
               <span className="text-[1.2rem]">{data?.original_title || 'N/A'}</span>
@@ -115,44 +198,19 @@ const DetailPage = () => {
 
             <div className="border-r-2 border-r-gray-900 w-full">
               <h1 className="text-[1.3rem] font-semibold mb-2">Original Language</h1>
-              <span className="text-[1.2rem]">{data?.original_language?.toUpperCase() || 'N/A'}</span>
-            </div>
-
-            <div className="border-r-2 border-r-gray-900 w-full">
-              <h1 className="text-[1.3rem] font-semibold mb-2">Budget</h1>
-              <span className="text-[1.2rem]">{formatCurrency(data?.budget)}</span>
+              <span className="text-[1.2rem]">{data?.original_language || 'N/A'}</span>
             </div>
 
             <div className="w-full">
-              <h1 className="text-[1.3rem] font-semibold mb-2">Revenue</h1>
-              <span className="text-[1.2rem]">{formatCurrency(data?.revenue)}</span>
+              <h1 className="text-[1.3rem] font-semibold mb-2">Budget</h1>
+              <span className="text-[1.2rem]">{formatCurrency(data?.budget)}</span>
             </div>
           </div>
+        </div>
 
-          <div>
-            <div className="w-full h-full">
-              <VideoPlay data={data} media_type={media_type} />
-            </div>
-          </div>
-
-          <div className="flex flex-col my-14">
-            <h1 className="text-[1.8rem] font-semibold my-4">Recommendations</h1>
-
-            <div className="flex overflow-x-auto scrollbar-thin">
-              <div className="flex">
-                {recommendationData?.map((data) => (
-                  <div key={data.id} className="relative w-56 min-h-[200px] bg-transparent rounded-lg overflow-hidden mr-8 ">
-                    <div>
-                      <div className="w-full h-full ">
-                        <img src={imageURL + data?.poster_path} className="w-full h-full object-cover rounded-lg mb-2 " alt={data?.name} />
-                      </div>
-                      <h1 className="text-[1.2rem] font-semibold">{data?.title}</h1>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+        <div className="relative">
+          <h1 className="text-[1.8rem] font-semibold mb-4">Trailer</h1>
+          <VideoPlay data={data} media_type={media_type} />
         </div>
       </div>
     </section>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { IoIosEye, IoIosEyeOff } from 'react-icons/io';
 import { loginUser } from '../../services/authService';
@@ -7,6 +7,7 @@ const LoginPage = () => {
   const [isShowPassword, setIsShowPassword] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
@@ -15,6 +16,7 @@ const LoginPage = () => {
 
     try {
       if (!email || !password) {
+        setError('Email and password are required.');
         return;
       }
 
@@ -22,17 +24,21 @@ const LoginPage = () => {
 
       if (login) {
         navigate('/', { replace: true });
+      } else {
+        setError('Login failed. Please check your credentials.');
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      setError('An error occurred during login.');
     }
   };
 
   return (
-    <section className="flex items-center justify-center min-h-screen ">
+    <section className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-xl p-8 bg-white rounded-lg shadow-lg">
         <h1 className="text-3xl font-bold text-center text-gray-800 my-6">Moviekun</h1>
-        <form className="w-full h-full space-y-8">
+        {error && <p className="text-red-500 text-center">{error}</p>}
+        <form className="w-full h-full space-y-8" onSubmit={handleLogin}>
           <div>
             <label className="mb-5 font-semibold text-[1.1rem]">Email</label>
             <input type="text" placeholder="Enter your email..." value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-4 py-5 bg-gray-200 rounded-lg border-2 focus:border-gray-900" />
@@ -46,7 +52,7 @@ const LoginPage = () => {
               <IoIosEyeOff className="absolute right-5 text-[1.6rem] cursor-pointer top-[70%] -translate-y-[70%]" onClick={() => setIsShowPassword((prev) => !prev)} />
             )}
           </div>
-          <button type="button" className="w-full py-4 rounded-lg text-white bg-gray-900" onClick={handleLogin}>
+          <button type="submit" className="w-full py-4 rounded-lg text-white bg-gray-900">
             Login
           </button>
 
